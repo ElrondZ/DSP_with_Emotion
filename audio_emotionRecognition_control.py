2b523897219a6c19f3163f1770ce4ae283b127b3
gary <runfeng_gai@163.com>
2022-12-14 09:17:01 -0500

    button

--- audio_emotionRecognition_control.py
+++ audio_emotionRecognition_control.py
@@ -2,16 +2,35 @@ import tkinter as tk
 
 
 def take_audio():
-    # 拍视频逻辑
     print("aaa")
+
+
+def pinpu():
+    print("频谱")
+
+
+def boxingtu():
+    print("波形图")
+
+
+def result():
+    print("情感结果")
 
 
 root = tk.Tk()
 # 标题
-root.title("audio")
+root.title("音频分析")
 # 大小
-root.minsize(800, 1000)
+root.minsize(600, 400)
 # 开始按钮
-start = tk.Button(root, text='Start', command=take_audio())
-start.pack()
+luyin = tk.Button(root, text='录音', height=30, width=20, command=take_audio())
+pinpu = tk.Button(root, text='生成频谱', height=30, width=20, command=pinpu())
+boxingtu = tk.Button(root, text='波形图', height=30, width=20, command=boxingtu())
+result = tk.Button(root, text='生成结果', height=30, width=20, command=result())
+
+luyin.pack(side=tk.LEFT)
+pinpu.pack(side=tk.LEFT)
+boxingtu.pack(side=tk.RIGHT)
+result.pack(side=tk.BOTTOM)
+
 root.mainloop()
